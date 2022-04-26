e38df427bbc01665c783c586b0d4fb6e5fb33865
Paula Zeman p.zeman@posteo.de <p.zeman@posteo.de>
2022-04-26 21:27:33 +0200

    corrected location type in birds model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_26_191741) do
+ActiveRecord::Schema.define(version: 2022_04_26_192540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2022_04_26_191741) do
     t.string "name"
     t.string "type"
     t.integer "price"
-    t.integer "location"
+    t.string "location"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
