541ac91608878e8c8d887ad2febb696a2ffd7d2d
Benjamin Nyaoro <nyaorobenjamin@gmail.com>
2022-04-28 20:32:02 +0200

    User data input validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :birds
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :phone_number, presence: true
+  validates :location, presence: true
 end
