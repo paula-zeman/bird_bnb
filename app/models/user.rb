c873fba0663d2e8934bdd0157ab2d338d6725424
Benjamin Nyaoro <nyaorobenjamin@gmail.com>
2022-04-28 20:28:09 +0200

    User data input validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :phone_number, presence: true
+  validates :location, presence: true
 end
