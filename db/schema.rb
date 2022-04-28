a23356a7d99e6a23f98711282f0c94f45ace5ec0
Benjamin Nyaoro <nyaorobenjamin@gmail.com>
2022-04-28 18:52:40 +0200

    Latest changes on the schema and FE views new page

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,10 @@ ActiveRecord::Schema.define(version: 2022_04_26_192540) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "phone_number"
+    t.string "location"
+    t.boolean "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
