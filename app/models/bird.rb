0f5644fdf1d2ab912de008029d18371f9cb2139c
Paula Zeman p.zeman@posteo.de <p.zeman@posteo.de>
2022-04-28 19:28:28 +0200

    improved type validation

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,6 +1,6 @@
 class Bird < ApplicationRecord
   belongs_to :user
   validates :name, presence: true, uniqueness: true
-  validates :type, presence: true, length: { minimum: 2 }
+  validates :type, inclusion: { in: ["Albatross", "Chicken", "Emu", "Flamingo", "Owl", "Parrot", "Penguin", "Turkey"] }
   validates :price, presence: true, numericality: { only_integer: true }
 end
