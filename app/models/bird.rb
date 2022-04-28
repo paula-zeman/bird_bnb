c4efafb9c891bda81e28ee0252ec22c1103f215a
Paula Zeman p.zeman@posteo.de <p.zeman@posteo.de>
2022-04-28 19:09:22 +0200

    Second bird migration and validations

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,3 +1,6 @@
 class Bird < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true, uniqueness: true
+  validates :type, presence: true, length: { minimum: 2 }
+  validates :price, presence: true, numericality: { only_integer: true }
 end
