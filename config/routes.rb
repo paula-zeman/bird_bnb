0451b23ad76457e3851605eebfcb52e4f395b2f0
Paula Zeman p.zeman@posteo.de <p.zeman@posteo.de>
2022-04-28 21:00:40 +0200

    typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resurces :birds do
+  resources :birds do
     resources :bookings, only: [:new, :create, :show]
   end
   resources :bookings, only: [:destroy]
