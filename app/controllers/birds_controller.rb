1631467bbaa63882dc7a404eafd038924552c090
Paula Zeman p.zeman@posteo.de <p.zeman@posteo.de>
2022-04-29 15:00:34 +0200

    added skip authentication to pages controller

---
+++ app/controllers/birds_controller.rb
@@ -0,0 +1,33 @@
+class BirdsController < ApplicationController
+  before_action :set_bird, only: [:show, :edit, :destoy, :update]
+
+  def index
+    @birds = Bird.all
+  end
+
+  def show
+  end
+
+  def new
+    @bird = Bird.new
+  end
+
+  def create
+    @bird = Bird.new(bird_params)
+    @bird.user = current_user
+    if @bird.save
+      redirect_to bird_path(@bird)
+    else
+      render :new
+    end
+  end
+  private
+
+  def set_bird
+    @bird = Bird.find(params[:id])
+  end
+
+  def bird_params
+    params.require(:bird).permit(:name, :type, :price, :location)
+  end
+end
