175e2a362bdc3081305d3a042655c4066cf86e60
Paula Zeman p.zeman@posteo.de <p.zeman@posteo.de>
2022-04-29 16:42:39 +0200

    finished controller actions with pundit authorization

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -2,7 +2,7 @@ class BirdsController < ApplicationController
   before_action :set_bird, only: [:show, :edit, :destoy, :update]
 
   def index
-    @birds = Bird.all
+    @birds = policy_scope(Restaurant)
   end
 
   def show
@@ -13,19 +13,37 @@ class BirdsController < ApplicationController
     authorize @bird
   end
 
+  def edit
+  end
+
   def create
     @bird = Bird.new(bird_params)
     @bird.user = current_user
+    authorize @bird
     if @bird.save
       redirect_to bird_path(@bird)
     else
-      render :new
+      render :new, notice: "Your bird could not be created."
     end
+  end
+
+  def update
+    if @bird.update(bird_params)
+      redirect_to @bird, notice: "Bird was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @bird.destroy
+    redirect_to birds_url, notice: "Bird was successfully destroyed."
   end
   private
 
   def set_bird
     @bird = Bird.find(params[:id])
+    authorize @bird
   end
 
   def bird_params
