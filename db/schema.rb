4336f1abf83641d20211f501d09b2d909b01c044
wayne <smudgeness@gmail.com>
2022-04-30 14:28:43 +0200

    fixing error in show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_30_111118) do
+ActiveRecord::Schema.define(version: 2022_04_30_121053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
