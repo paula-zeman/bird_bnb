756b2c1b9c79fed1b9072e45d3285438cc16d050
Paula Zeman p.zeman@posteo.de <p.zeman@posteo.de>
2022-05-03 21:19:37 +0200

    controller methods 1

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,19 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :edit, :destoy, :update]
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+    authorize @booking
+  end
+
 end
