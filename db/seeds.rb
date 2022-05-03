271713521c795898887e89c4a14ddc6adc1956c8
Paula Zeman p.zeman@posteo.de <p.zeman@posteo.de>
2022-05-03 17:46:10 +0200

    Seeds with images

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,20 +14,26 @@ puts "Database clean now!!"
 
 puts "Creating users"
 user1 = User.create!(email: "abc@blabla.com", password: "123456")
-user1 = User.create!(email: "user@blabla.com", password: "123456")
-user1 = User.create!(email: "admin@blabla.com", password: "123456")
-user1 = User.create!(email: "user1@blabla.com", password: "123456")
+user2 = User.create!(email: "user@blabla.com", password: "123456")
+user3 = User.create!(email: "admin@blabla.com", password: "123456")
+user4 = User.create!(email: "user1@blabla.com", password: "123456")
 puts "Users created successfully"
 
 puts "Creating Birds"
-new_bird = Bird.create!(name: "Bird 1", breed: "Flamingo", price: 10, location: "Berlin", user_id: 1)
-new_bird = Bird.create!(name: "Bird 2", breed: "Albatross", price: 20, location: "Frankfurt", user_id: 1)
-new_bird = Bird.create!(name: "Bird 3", breed: "Chicken", price: 30, location: "Leipzig", user_id: 1)
-new_bird = Bird.create!(name: "Bird 4", breed: "Emu", price: 25, location: "London", user_id: 1)
-new_bird = Bird.create!(name: "Bird 5", breed: "Owl", price: 15, location: "Paris", user_id: 1)
-new_bird = Bird.create!(name: "Bird 6", breed: "Parrot", price: 18, location: "Stockholm", user_id: 1)
-new_bird = Bird.create!(name: "Bird 7", breed: "Penguin", price: 23, location: "Sofia", user_id: 1)
-new_bird = Bird.create!(name: "Bird 8", breed: "Turkey", price: 33, location: "Cologne", user_id: 1)
+new_bird = Bird.create!(name: "Rosalie", breed: "Chicken", price: 10, location: "Berlin", user_id: 1)
+new_bird.image.attach(io: File.open(Rails.root.join('app/assets/images/chicken_249474721.jpg')),
+filename: 'chicken_249474721.jpg')
+second_bird = Bird.create!(name: "Rufus", breed: "Flamingo", price: 20, location: "Frankfurt", user_id: 2)
+second_bird.image.attach(io: File.open(Rails.root.join('app/assets/images/rosa-flamingo.jpg')),
+filename: 'rosa-flamingo.jpg')
+third_bird = Bird.create!(name: "Olav", breed: "Penguin", price: 30, location: "Leipzig", user_id: 3)
+third_bird.image.attach(io: File.open(Rails.root.join('app/assets/images/1200px-SGI-2016-South_Georgia_(Fortuna_Bay)–King_penguin_(Aptenodytes_patagonicus)_04.jpg')),
+filename: '1200px-SGI-2016-South_Georgia_(Fortuna_Bay)–King_penguin_(Aptenodytes_patagonicus)_04.jpg')
+# new_bird = Bird.create!(name: "Bird 4", breed: "Emu", price: 25, location: "London", user_id: 1)
+# new_bird = Bird.create!(name: "Bird 5", breed: "Owl", price: 15, location: "Paris", user_id: 1)
+# new_bird = Bird.create!(name: "Bird 6", breed: "Parrot", price: 18, location: "Stockholm", user_id: 1)
+# new_bird = Bird.create!(name: "Bird 7", breed: "Penguin", price: 23, location: "Sofia", user_id: 1)
+# new_bird = Bird.create!(name: "Bird 8", breed: "Turkey", price: 33, location: "Cologne", user_id: 1)
 
 puts "Birds created successfully"
 
