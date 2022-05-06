506e131e5234cc7b004510f523d4b01287904e9a
Benjamin Nyaoro <nyaorobenjamin@gmail.com>
2022-05-06 14:02:43 +0200

    Schema modified

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2022_05_05_185733) do
+ActiveRecord::Schema.define(version: 2022_05_05_202208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,9 +52,9 @@ ActiveRecord::Schema.define(version: 2022_05_05_185733) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
-    t.boolean "status", default: false
     t.float "latitude"
     t.float "longitude"
+    t.boolean "status", default: false
     t.index ["user_id"], name: "index_birds_on_user_id"
   end
 
